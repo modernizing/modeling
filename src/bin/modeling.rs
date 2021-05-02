0bc4183b778345c2fa9eb5803c7ccc016a2ea128
Phodal Huang <h@phodal.com>
2021-05-02 21:13:41 +0800

    feat: create api for mermaid

--- src/bin/modeling.rs
+++ src/bin/modeling.rs
@@ -1,6 +1,6 @@
 use std::{env, fs};
 use modeling::by_dir;
-use modeling::render::PlantUmlRender;
+use modeling::render::{PlantUmlRender, MermaidRender};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,10 +8,23 @@ fn main() {
     if args.len() > 1 {
         path = args[1].clone();
     }
+    let mut design_type = "puml".to_string();
+    if args.len() > 2 {
+        design_type = args[2].clone();
+    }
 
     println!("Input path: {:?}", path.clone());
     let classes = by_dir(path.as_str());
-    let uml = PlantUmlRender::render(&classes);
 
-    let _ = fs::write("modeling.puml", uml);
+    match design_type.as_str() {
+        "mermaid" => {
+            let uml = MermaidRender::render(&classes);
+            let _ = fs::write("modeling.mermaid", uml);
+        }
+        &_ => {
+            let uml = PlantUmlRender::render(&classes);
+            let _ = fs::write("modeling.puml", uml);
+        }
+    }
+
 }
