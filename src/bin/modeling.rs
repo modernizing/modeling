4783323be69e9ea7997891ad000148ce335e2edc
Phodal Huang <h@phodal.com>
2021-06-10 11:00:19 +0800

    feat(cli): add parser for delimiter

--- src/bin/modeling.rs
+++ src/bin/modeling.rs
@@ -1,32 +1,40 @@
 use std::fs;
 
 use clap::{AppSettings, Clap};
+use structopt::StructOpt;
 
 use modeling::by_dir;
 use modeling::render::{MermaidRender, PlantUmlRender};
 
-#[derive(Clap)]
+#[derive(StructOpt, Debug, Clap)]
 #[clap(version = "1.0", author = "Inherd Group <group@inherd.org>")]
 #[clap(setting = AppSettings::ColoredHelp)]
+#[structopt(name = "basic")]
 struct Opts {
+    /// input dir
     #[clap(short, long, default_value = ".")]
-    source_dir: String,
+    input: String,
 
+    /// mermaid or puml
     #[clap(short, long, default_value = "puml")]
     output_type: String,
 
-    #[clap(long)]
+    // filter dir
+    #[clap(long, short, use_delimiter = true)]
     packages: Vec<String>,
 
-    #[clap(long)]
+    // filter suffixes
+    #[clap(long, short, use_delimiter = true)]
     suffixes: Vec<String>,
 }
 
 fn main() {
     let opts: Opts = Opts::parse();
-    println!("Input path: {:?}", opts.source_dir);
+    println!("Input path: {:?}", opts.input);
+    println!("packages: {:?}", opts.packages);
+    println!("suffixes: {:?}", opts.suffixes);
 
-    let classes = by_dir(opts.source_dir);
+    let classes = by_dir(opts.input);
 
     match opts.output_type.as_str() {
         "mermaid" => {
