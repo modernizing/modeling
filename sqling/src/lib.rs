233bc008ba203a88ae88956b72d228f26de67031
Phodal Huang <h@phodal.com>
2021-06-10 18:44:03 +0800

    feat: init sqling

--- sqling/src/lib.rs
+++ sqling/src/lib.rs
@@ -1,7 +1,25 @@
+use sqlparser::dialect::GenericDialect;
+use sqlparser::parser::Parser;
+
+pub fn sqling() {
+    let sql = "SELECT a, b, 123, myfunc(b) \
+           FROM table_1 \
+           WHERE a > b AND b < 100 \
+           ORDER BY a DESC, b";
+
+    let dialect = GenericDialect {}; // or AnsiDialect, or your own dialect ...
+
+    let ast = Parser::parse_sql(&dialect, sql).unwrap();
+
+    println!("AST: {:?}", ast);
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::sqling;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        sqling();
     }
 }
