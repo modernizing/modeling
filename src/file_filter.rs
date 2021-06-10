8d7e97288a6d6f30477193e115e8b3b8f0fed7dc
Phodal Huang <h@phodal.com>
2021-06-10 09:46:28 +0800

    feat: add filter by suffix func

--- src/file_filter.rs
+++ src/file_filter.rs
@@ -1,7 +1,39 @@
-fn filter_by_packages(path: String, packages: Vec<String>) -> bool {
+use std::path::PathBuf;
+use std::ffi::OsStr;
+
+pub fn filter_by_packages(path: PathBuf, packages: Vec<String>) -> bool {
     return false;
 }
 
-fn filter_by_suffix(path: String, suffixes: Vec<String>) -> bool {
+pub fn filter_by_suffix(path: PathBuf, suffixes: Vec<String>) -> bool {
+    if let None = path.file_name() {
+        return false;
+    }
+    let file_name = path.file_name().unwrap().to_str().unwrap();
+
+    for suffix in suffixes.iter() {
+        if file_name.contains(suffix) {
+            return true;
+        }
+    }
+
     return false;
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+    use crate::file_filter::filter_by_suffix;
+
+    #[test]
+    fn should_filter_by_file_name_suffix() {
+        let buf = PathBuf::new()
+            .join("model")
+            .join("CustomModel.java");
+
+        let suffixes = vec!["Model".to_string()];
+
+        assert!(filter_by_suffix(buf, suffixes));
+    }
 }
