bf20c2d5ebfd61180a8c4c8e28b0553273c418c6
Phodal Huang <h@phodal.com>
2021-06-10 18:59:15 +0800

    refactor: testing query

--- sqling/src/lib.rs
+++ sqling/src/lib.rs
@@ -1,13 +1,8 @@
-use sqlparser::dialect::GenericDialect;
+use sqlparser::dialect::{GenericDialect, MySqlDialect};
 use sqlparser::parser::Parser;
 
-pub fn sqling() {
-    let sql = "SELECT a, b, 123, myfunc(b) \
-           FROM table_1 \
-           WHERE a > b AND b < 100 \
-           ORDER BY a DESC, b";
-
-    let dialect = GenericDialect {}; // or AnsiDialect, or your own dialect ...
+pub fn sqling(sql: &str) {
+    let dialect = MySqlDialect {}; // or AnsiDialect, or your own dialect ...
 
     let ast = Parser::parse_sql(&dialect, sql).unwrap();
 
@@ -20,6 +15,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        sqling();
+        sqling("SELECT a, b, 123, myfunc(b) FROM table_1 WHERE a > b AND b < 100 ORDER BY a DESC, b");
     }
 }
