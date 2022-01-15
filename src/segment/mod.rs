24ff7f830f0da706ff69da553626c3d2c29d8ae6
Phodal Huang <h@phodal.com>
2022-01-15 10:06:43 +0800

    refactor: move code from test to production

--- src/segment/mod.rs
+++ src/segment/mod.rs
@@ -1,24 +1,28 @@
 pub mod camelcase_tok;
 
+use tokenizers::{OffsetReferential, OffsetType, PreTokenizedString, PreTokenizer};
+use crate::segment::camelcase_tok::CamelCaseTok;
+
+pub fn segment_camelcase() -> Vec<&str> {
+    let pretok = CamelCaseTok::default();
+
+    let mut pretokenized = PreTokenizedString::from("HierarchyId");
+    pretok.pre_tokenize(&mut pretokenized).unwrap();
+
+    pretokenized
+        .get_splits(OffsetReferential::Original, OffsetType::Byte)
+        .into_iter()
+        .map(|(s, _o, _)| (s))
+        .collect::<Vec<&str>>()
+}
+
 
 #[cfg(test)]
 mod tests {
-    use tokenizers::{OffsetReferential, OffsetType, PreTokenizedString, PreTokenizer};
-    use crate::segment::camelcase_tok::CamelCaseTok;
+    use crate::segment::segment_camelcase;
 
     #[test]
     fn should_segmentation() {
-        let pretok = CamelCaseTok::default();
-
-        let mut pretokenized = PreTokenizedString::from("HierarchyId");
-        pretok.pre_tokenize(&mut pretokenized).unwrap();
-
-        let vec = pretokenized
-            .get_splits(OffsetReferential::Original, OffsetType::Byte)
-            .into_iter()
-            .map(|(s, _o, _)| (s))
-            .collect::<Vec<&str>>();
-
-        assert_eq!(vec!["Hierarchy", "Id"], vec);
+        assert_eq!(vec!["Hierarchy", "Id"], segment_camelcase());
     }
 }
