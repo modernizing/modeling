aca489ab212e8d999be7ea9af56d009b85e098b5
Phodal Huang <h@phodal.com>
2022-01-15 10:13:14 +0800

    refactor: extract semgnet method

--- src/segment/mod.rs
+++ src/segment/mod.rs
@@ -3,26 +3,32 @@ pub mod camelcase_tok;
 use tokenizers::{OffsetReferential, OffsetType, PreTokenizedString, PreTokenizer};
 use crate::segment::camelcase_tok::CamelCaseTok;
 
-pub fn segment_camelcase() -> Vec<&str> {
+pub fn segment(str: &str) -> Vec<String> {
+    segment_camelcase(str)
+}
+
+pub fn segment_camelcase(str: &str) -> Vec<String> {
     let pretok = CamelCaseTok::default();
 
-    let mut pretokenized = PreTokenizedString::from("HierarchyId");
+    let mut pretokenized = PreTokenizedString::from(str);
     pretok.pre_tokenize(&mut pretokenized).unwrap();
 
-    pretokenized
+    let vec = pretokenized
         .get_splits(OffsetReferential::Original, OffsetType::Byte)
         .into_iter()
-        .map(|(s, _o, _)| (s))
-        .collect::<Vec<&str>>()
+        .map(|(s, _o, _)| (s.to_string()))
+        .collect::<Vec<String>>();
+
+    vec
 }
 
 
 #[cfg(test)]
 mod tests {
-    use crate::segment::segment_camelcase;
+    use crate::segment::{segment};
 
     #[test]
     fn should_segmentation() {
-        assert_eq!(vec!["Hierarchy", "Id"], segment_camelcase());
+        assert_eq!(vec!["Hierarchy".to_string(), "Id".to_string()], segment("HierarchyId"));
     }
 }
